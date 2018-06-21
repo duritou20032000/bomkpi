b75a351628df8586c31b1ee83e88e4a1a65586aa
leohorry <leohorry@163.com>
2018-06-21 15:46:26 +0800

    还没限制同仓同一个柜台分配一个订单一次。

--- src/main/java/com/mr/bomkpi/controller/TaskController.java
+++ src/main/java/com/mr/bomkpi/controller/TaskController.java
@@ -1,5 +1,6 @@
 package com.mr.bomkpi.controller;
 
+import com.mr.bomkpi.entity.Task;
 import com.mr.bomkpi.repository.TaskCounterRepository;
 import com.mr.bomkpi.repository.TaskOrderRepository;
 import com.mr.bomkpi.repository.TaskRepository;
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class TaskController {
@@ -21,6 +24,9 @@ public class TaskController {
 
     @GetMapping("/task/getTasks")
     public String getTasks(Model model){
+
+        List<Task> tasks = taskRepository.findAll();
+        model.addAttribute("tasks",tasks);
       return "task/list";
     }
 
