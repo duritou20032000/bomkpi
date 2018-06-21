480de04dcb9c64b4c8cff826b69ef2565249bf8f
leohorry <leohorry@163.com>
2018-06-21 17:02:27 +0800

    准备绩效页面，并定准备时绩效扫描

--- src/main/java/com/mr/bomkpi/controller/KPIController.java
+++ src/main/java/com/mr/bomkpi/controller/KPIController.java
@@ -1,13 +1,43 @@
 package com.mr.bomkpi.controller;
 
+import com.mr.bomkpi.entity.KPIcaculation;
+import com.mr.bomkpi.entity.KPIdata;
+import com.mr.bomkpi.entity.KPIotherData;
+import com.mr.bomkpi.repository.KpiCaculationRepository;
+import com.mr.bomkpi.repository.KpidataRepository;
+import com.mr.bomkpi.repository.KpiotherDataRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class KPIController {
+
+    @Autowired
+    private KpidataRepository kpidataRepository;
+
+    @Autowired
+    private KpiCaculationRepository kpiCaculationRepository;
+
+    @Autowired
+    private KpiotherDataRepository kpiotherDataRepository;
+
     @GetMapping("/kpi/getKpis")
     public String getKpis(Model model){
-        return "/kpi/list";
+
+        List<KPIdata> recorders =  kpidataRepository.findAll();
+
+        List<KPIotherData> oRecorders = kpiotherDataRepository.findAll();
+
+        List<KPIcaculation> cRecorders = kpiCaculationRepository.findAll();
+
+        model.addAttribute("recorders",recorders);
+        model.addAttribute("oRecorders",oRecorders);
+        model.addAttribute("cRecorders",cRecorders);
+
+        return "kpi/list";
     }
 }
---
+++ src/main/java/com/mr/bomkpi/service/KPIService.java
@@ -0,0 +1,25 @@
+package com.mr.bomkpi.service;
+
+import com.mr.bomkpi.repository.KpiCaculationRepository;
+import com.mr.bomkpi.repository.KpidataRepository;
+import com.mr.bomkpi.repository.KpiotherDataRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class KPIService {
+
+    public static Logger logger = LoggerFactory.getLogger(KPIService.class);
+
+
+
+
+
+
+
+
+
+
+}
